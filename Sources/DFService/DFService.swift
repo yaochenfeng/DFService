54d808cbcc27fc18a92866bb5d042a48802a1d44
yaochenfeng <yaochenfeng@1024ul.com>
2023-06-19 13:55:56 +0800

    feat: add package

---
+++ Sources/DFService/DFService.swift
@@ -0,0 +1,6 @@
+public struct DFService {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/DFServiceTests/DFServiceTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import DFService
+
+final class DFServiceTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(DFService().text, "Hello, World!")
+    }
+}
