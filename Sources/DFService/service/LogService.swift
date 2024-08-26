bfcbcc8c15f67ea756082224c1c223f9c3ccd345
yaochenfeng <282696845@qq.com>
2024-08-26 22:55:53 +0800

    feat: 日志完善

--- Sources/DFService/service/LogService.swift
+++ Sources/DFService/service/LogService.swift
@@ -1,6 +1,52 @@
 public protocol DFLogHandle {
-    func debug(_ msg: String)
+    func log(level: LogService.Level,
+             message:  @autoclosure () -> String,
+             file: String,
+             function: String,
+             line: UInt)
+    
+    var logLevel: LogService.Level { get }
 }
+
+public extension DFLogHandle {
+    func log(level: LogService.Level,
+             message: @autoclosure () -> String,
+             file: String = #fileID,
+             function: String = #function,
+             line: UInt = #line) {
+        guard logLevel >= level else { return }
+        debugPrint("\(level) - \(file):\(line) - \(function) - \(message())")
+    }
+    
+    func debug(_ message: @autoclosure () -> String,
+             file: String = #fileID,
+             function: String = #function,
+             line: UInt = #line) {
+        self.log(level: .debug, message: message(), file: file, function: function, line: line)
+    }
+    
+    func info(_ message: @autoclosure () -> String,
+              file: String = #fileID,
+              function: String = #function,
+              line: UInt = #line) {
+         self.log(level: .info, message: message(), file: file, function: function, line: line)
+     }
+    
+    func warning(_ message: @autoclosure () -> String,
+              file: String = #fileID,
+              function: String = #function,
+              line: UInt = #line) {
+         self.log(level: .warning, message: message(), file: file, function: function, line: line)
+     }
+    
+    func error(_ message: @autoclosure () -> String,
+              file: String = #fileID,
+              function: String = #function,
+              line: UInt = #line) {
+         self.log(level: .error, message: message(), file: file, function: function, line: line)
+     }
+}
+
 
 public struct LogService: DFApiService {
     public static var defaultValue: DFLogHandle {
@@ -8,8 +54,17 @@ public struct LogService: DFApiService {
     }
 }
 
+public extension LogService {
+    enum Level: Comparable {
+        case debug
+        case info
+        case warning
+        case error
+    }
+}
+
 extension MockApiServiceImpl: DFLogHandle {
-    func debug(_ msg: String) {
-        debugPrint(msg)
+    var logLevel: LogService.Level {
+        .debug
     }
 }
