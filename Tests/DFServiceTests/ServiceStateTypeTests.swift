352368c24efa8ac580923036918efb4402e7c944
yaochenfeng <282696845@qq.com>
2025-06-07 16:05:11 +0800

    feat: 添加ServicePromise处理异步任务

--- Tests/DFServiceTests/ServiceStateTypeTests.swift
+++ Tests/DFServiceTests/ServiceStateTypeTests.swift
@@ -4,6 +4,42 @@ import XCTest
 
 final class ServiceStateTypeTests: XCTestCase {
     let store = ServiceStore(state: CounterState(name: "demo"))
+    
+    func testInitialState() {
+        let initialState = CounterState(name: "init", count: 5, isLoading: false)
+        XCTAssertEqual(initialState.name, "init")
+        XCTAssertEqual(initialState.count, 5)
+        XCTAssertFalse(initialState.isLoading)
+    }
+
+    func testReducerChangeName() {
+        let state = CounterState(name: "old", count: 0, isLoading: false)
+        let newState = CounterState.reducer(state: state, action: .change("new"))
+        XCTAssertEqual(newState?.name, "new")
+        XCTAssertEqual(newState?.count, 0)
+    }
+
+    func testReducerIncrementDecrement() {
+        var state = CounterState(name: "test", count: 0, isLoading: false)
+        state = CounterState.reducer(state: state, action: .increment)!
+        XCTAssertEqual(state.count, 1)
+        state = CounterState.reducer(state: state, action: .decrement)!
+        XCTAssertEqual(state.count, 0)
+    }
+
+    func testReducerSetLoading() {
+        let state = CounterState(name: "test", count: 0, isLoading: false)
+        let newState = CounterState.reducer(state: state, action: .setLoading(true))
+        XCTAssertTrue(newState?.isLoading ?? false)
+    }
+
+    func testReducerLoadDataReturnsNil() {
+        let state = CounterState(name: "test", count: 0, isLoading: false)
+        let result = CounterState.reducer(state: state, action: .loadData)
+        XCTAssertNil(result)
+    }
+
+   
     func testExample() async throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
