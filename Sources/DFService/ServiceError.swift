75974b5befc56052773ed2d05868b5cd3ceb9990
yaochenfeng <282696845@qq.com>
2025-06-15 16:05:21 +0800

    feat: ServiceError 添加错误信息

--- Sources/DFService/ServiceError.swift
+++ Sources/DFService/ServiceError.swift
@@ -3,6 +3,9 @@
 ///
 /// - api: Indicates an API error with an associated error code and message.
 /// - notImplemented: Indicates that a feature or method is not yet implemented. The default value includes the file and function name.
+
+import Foundation
+
 public enum ServiceError: Error {
     /// API 错误，包含错误代码和消息
     /// - Parameters:
@@ -12,7 +15,7 @@ public enum ServiceError: Error {
     case api(code: Int, message: String, detail: String? = nil)
 
     /// 未实现
-    case notImplemented(String = #fileID + " " + #function)
+    case notImplemented(String = #function)
     /// 自定义错误
     case custom(Error)
 
@@ -21,6 +24,43 @@ public enum ServiceError: Error {
             return serviceError
         } else {
             return .custom(error)
+        }
+    }
+}
+
+/// 扩展 ServiceError 以提供错误描述
+extension ServiceError: CustomStringConvertible, CustomDebugStringConvertible {
+    public var errorCode: Int {
+        switch self {
+        case .api(let code, _, _):
+            return code
+        case .notImplemented:
+            return 0  // 未实现的错误没有特定的错误代码
+        case .custom(let error):
+            return (error as NSError).code  // 使用 NSError 的代码
+        }
+    }
+    public var description: String {
+        switch self {
+        case .api(_, let message, _):
+            return message
+        case .notImplemented(let location):
+            return "未实现: \(location)"
+        case .custom(let error):
+            return error.localizedDescription
+        }
+    }
+
+    public var debugDescription: String {
+
+        switch self {
+        case .api(let code, let message, let detail):
+            return
+                "ServiceError api - Code: \(code), Message: \(message), Detail: \(detail ?? "No detail")"
+        case .notImplemented(let location):
+            return "ServiceError - Location: \(location)"
+        case .custom(let error):
+            return "ServiceError - \(error.localizedDescription)"
         }
     }
 }
